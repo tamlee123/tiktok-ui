a2b28c2bb8bbd97e27862ca7f49cca0bda2d3478
tam <cherykim123@gmail.com>
2022-06-30 00:59:25 -0700

    Router/Layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
 function App() {
     return (
-        <div className="App">
-            <h1>React app</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        //initial variable for a component we use Pascal notation
+                        const Page = route.component;
+                        return <Route key={index} path={route.path} element={<Page />} />;
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
