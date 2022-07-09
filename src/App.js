b0e45099bca5d355cabfb8555947828179e576ce
tam <cherykim123@gmail.com>
2022-07-08 20:06:41 -0700

    analyze layout, build layout and routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
+import { Fragment } from 'react';
 function App() {
     return (
         <Router>
@@ -7,8 +9,26 @@ function App() {
                 <Routes>
                     {publicRoutes.map((route, index) => {
                         //initial variable for a component we use Pascal notation
+                        //Fragment tag is just for contain
                         const Page = route.component;
-                        return <Route key={index} path={route.path} element={<Page />} />;
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
                     })}
                 </Routes>
             </div>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,17 @@
+import { HeaderOnly } from '~/components/Layout';
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+//public routes
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/following', component: Following },
+    { path: '/frofile', component: Profile },
+    { path: '/search', component: Search, layout: null },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
 ];
 const privateRoutes = [];
 export { publicRoutes, privateRoutes };
